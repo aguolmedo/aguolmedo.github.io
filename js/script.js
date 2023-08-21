75561d32cfec9eb7c8d2f5cebc8a90313adc2ac1
aguolmedo <aguolmedofernandez@gmail.com>
2023-08-21 20:03:06 -0300

    now working

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const fileList = document.getElementById("file-list");
-  const folderPath = "https://api.github.com/repos/aguolmedo/aguolmedo.github.io/contents/pdfs"; 
+  const folderPath =
+    "https://api.github.com/repos/aguolmedo/aguolmedo.github.io/contents/pdfs";
 
   fetch(folderPath)
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const htmlDocument = parser.parseFromString(data, "text/html");
-            const links = htmlDocument.querySelectorAll("a");
-            
-            links.forEach(link => {
-                const listItem = document.createElement("li");
-                const anchor = document.createElement("a");
-                anchor.href = folderPath + link.getAttribute("href");
-                anchor.textContent = link.textContent;
-                listItem.appendChild(anchor);
-                fileList.appendChild(listItem);
-            });
-        })
-        .catch(error => {
-            console.error("Error fetching file list:", error);
-        });
-})
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((file) => {
+        const listItem = document.createElement("li");
+        const anchor = document.createElement("a");
+        anchor.href = file.download_url;
+        anchor.textContent = file.name;
+        listItem.appendChild(anchor);
+        fileList.appendChild(listItem);
+      });
+    });
+});
