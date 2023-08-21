65c004e24c2ec03c5da9876a0e1fab2944555b26
aguolmedo <aguolmedofernandez@gmail.com>
2023-08-21 11:04:16 -0300

    pdf list

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const fileList = document.getElementById("file-list");
+  const folder = "../pdfs/";
+
+  fetch(folderPath)
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser();
+            const htmlDocument = parser.parseFromString(data, "text/html");
+            const links = htmlDocument.querySelectorAll("a");
+            
+            links.forEach(link => {
+                const listItem = document.createElement("li");
+                const anchor = document.createElement("a");
+                anchor.href = folderPath + link.getAttribute("href");
+                anchor.textContent = link.textContent;
+                listItem.appendChild(anchor);
+                fileList.appendChild(listItem);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching file list:", error);
+        });
+})
