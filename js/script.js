c40ab082100707537d04747e4b8b4913ca64f65c
aguolmedo <aguolmedofernandez@gmail.com>
2024-08-21 20:43:49 +0200

    Remove shit

--- js/script.js
+++ js/script.js
@@ -17,26 +17,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const imageGrid = document.getElementById("image-grid");
-  const folderPath =
-    "https://api.github.com/repos/aguolmedo/aguolmedo.github.io/contents/photos";
-
-  fetch(folderPath)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((image) => {
-        if (
-          image.type === "file" &&
-          image.name.match(/\.(jpg|jpeg|png|gif)$/)
-        ) {
-          const imageItem = document.createElement("div");
-          const img = document.createElement("img");
-          img.src = image.download_url;
-          img.alt = image.name;
-          imageItem.appendChild(img);
-          imageGrid.appendChild(imageItem);
-        }
-      });
-    });
-});
